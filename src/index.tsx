d3ab3cbbefd892ae14b448e1f87102a09bc4bfa8
Umberto Lanno <umberto.lanno@gmail.com>
2023-09-06 13:30:22 +0200

    chore: edit version
    BREAKING CHANGES: manual export of all types in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
 export { ReanimatedTabView } from './components/ReanimatedTabView';
 export { SceneMap } from './utils/SceneMap';
-export * from './types/types';
+export type {
+  SceneMapRoutes,
+  SceneProps,
+  NavigationState,
+  Route,
+  RenderTabsParams,
+} from './types/types';
